ef0def83bbb2fceb95b6d4f0ca633b327840e350
chetan patil <pchetan908@gmail.com>
2024-06-25 00:14:15 +0530

    addition program commit

---
+++ JavaPrograms/src/collection/ArithmaticOperation.java
@@ -0,0 +1,26 @@
+package collection;
+
+public class ArithmaticOperation {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+
+        int b = 10;
+
+        int c = a*b;
+
+
+        //this is the simple mltiplication program
+
+        System.out.println("the ans is:"+c);
+
+        //addition
+
+        int z = a+b;
+
+        System.out.println("the addition of two number are:"+z);
+
+
+    }
+}
--- JavaPrograms/src/collection/FirstProgram.java
+++ JavaPrograms/src/collection/FirstProgram.java
@@ -4,7 +4,7 @@ public class FirstProgram {
 
 
     //this is ths first program in java when you just print a text in your console thro system.out.println
-    
+
     public static void main(String[] args) {
 
         System.out.println("Hello World");
