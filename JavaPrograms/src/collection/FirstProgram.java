08fb0e30de9ef47e883a6a56a2f3a17fa7e62359
chetan patil <pchetan908@gmail.com>
2024-06-25 00:06:50 +0530

    hello world commit

---
+++ JavaPrograms/src/collection/FirstProgram.java
@@ -0,0 +1,12 @@
+package collection;
+
+public class FirstProgram {
+
+
+    //this is ths first program in java when you just print a text in your console thro system.out.println
+    
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
