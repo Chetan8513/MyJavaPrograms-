ee5fb0a4dadca21967979ce6b95b863d8bbfe86e
chetan patil <pchetan908@gmail.com>
2024-06-25 14:04:13 +0530

    findchar

---
+++ JavaPrograms/src/collection/FindCharacter.java
@@ -0,0 +1,28 @@
+package collection;
+
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+public class FindCharacter {
+
+    public static void main(String[] args) {
+
+        String name = "chetan";
+
+        char ch = 'e';
+
+      int a  =  findChar(name,ch);
+
+        System.out.println(a);
+    }
+
+
+    public static int findChar(String name, char ch) {
+
+        OptionalInt num = IntStream.range(0, name.length()).filter(i -> name.charAt(i) == ch).findFirst();
+
+        return num.orElse(-1);
+    }
+
+
+}
